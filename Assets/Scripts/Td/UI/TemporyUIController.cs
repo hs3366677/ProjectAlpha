b77d93c4f49b01afd8d1aba7976539714854bbd6
Dorochi <hs3366677@hotmail.com>
2018-08-14 01:46:04 +0800

    projectalpha
    project

---
+++ Assets/Scripts/Td/UI/TemporyUIController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TowerDefense.Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemporyUIController : MonoBehaviour {
+
+    public Button btnLoad;
+    public Button btnSave;
+    public Button btnReset;
+    public Button btnStartPause;
+
+    // Use this for initialization
+    void Start () {
+        btnLoad.onClick.AddListener(OnClickLoad);
+        btnSave.onClick.AddListener(OnClickSave);
+        btnReset.onClick.AddListener(OnClickReset);
+        btnStartPause.onClick.AddListener(OnClickPause);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnClickLoad()
+    {
+        GameManager.instance.LoadData();
+    }
+
+    void OnClickSave()
+    {
+        GameManager.instance.SaveData();
+    }
+
+    void OnClickReset()
+    {
+        GameManager.instance.ResetLevel();
+    }
+    
+    enum Status
+    {
+        Build,
+        Start,
+        Pause
+    }
+    Status currentStatus;
+    void OnClickPause()
+    {
+        if (currentStatus == Status.Pause)
+        {
+            currentStatus = Status.Start;
+            btnStartPause.GetComponentInChildren<Text>().text = "Start";
+        }
+        else if (currentStatus == Status.Build)
+        {
+            currentStatus = Status.Start;
+            btnStartPause.GetComponentInChildren<Text>().text = "Start";
+        }
+        else if (currentStatus == Status.Start)
+        {
+            currentStatus = Status.Pause;
+            btnStartPause.GetComponentInChildren<Text>().text = "Pause";
+        }
+
+    }
+}
